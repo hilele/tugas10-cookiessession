4e56e3da770821acdf47007dbceff8cd396900d7
hilele <hilmawan.ki@gmail.com>
2017-09-22 21:00:18 +0700

    mengumpulkan tugas10

---
+++ 10_C2_08829/code3.php
@@ -0,0 +1,18 @@
+<?php
+//buat class smartphone
+class smartphone{
+ //property dengan hak akses protected
+ protected $pembuat = "China";
+}
+//buat class tablet
+class tablet extends smartphone{
+ public function tampilkan_pembuat(){
+  return $this->pembuat;
+ }
+}
+//buat objek dari class tablet
+$tablet_baru = new tablet();
+//jalankan method
+//akses dari turunan
+echo $tablet_baru->tampilkan_pembuat();
+?>
---
+++ 10_C2_08829/praktikum8.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 14 Sep 2016 pada 15.24
+-- Versi Server: 10.1.9-MariaDB
+-- PHP Version: 5.6.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `praktikum8`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `level` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `level`) VALUES
+(11, 'andi_pr', '23eewfgt', 'user'),
+(12, 'hardisana_k', 'kekeHardi77', 'admin'),
+(14, 'ahmad', 'ahmad', 'user'),
+(16, 'harist', 'harist675', 'user'),
+(17, 'handoko', 'handokoRizal', 'admin'),
+(18, 'andiwingi', 'zikrizhorif66', 'user'),
+(21, 'hilmawan', '12345', 'user'),
+(22, 'agus_sudrajat', 'sakasnf', 'user'),
+(23, 'muhammad', 'bismillah183', 'user'),
+(24, 'anisa', '123', 'user');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
